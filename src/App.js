290a086728190d71c5b69f71540e325a1b7aefaa
luco17 <lucodonoghue@gmail.com>
2021-08-09 08:02:13 +0100

    added source code footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 import Figure from './components/Figure';
+import githubLogo from './githubLogo.png';
 import Mask from './components/Mask';
 import StateBanner from './components/StateBanner';
 
@@ -34,23 +35,33 @@ function App() {
   }
 
   return(
-    <div id="game">
-      
-      <h1>{gameTitle}</h1>
-      <h2>Guess the rapper</h2>
-      <Figure nWrong={nWrong} />
-      {/* <h2>{rapper}</h2> */}
-      <h3>Number of guesses remaining: {6 - nWrong}</h3>
-      <Mask rapper={rapper} guesses={guesses} />
-      <StateBanner 
-        winningWord={winningWord} 
-        guesses={guesses} 
-        nWrong={nWrong}
-        rapper={rapper}
-        winChecker={winChecker}
-        handleGuess={handleGuess}
-      />
+    <div>
+      <div id="game">
+        
+        <h1>{gameTitle}</h1>
+        <h2>Guess the rapper</h2>
+        <Figure nWrong={nWrong} />
+        {/* <h2>{rapper}</h2> */}
+        <h3>Number of guesses remaining: {6 - nWrong}</h3>
+        <Mask rapper={rapper} guesses={guesses} />
+        <StateBanner 
+          winningWord={winningWord} 
+          guesses={guesses} 
+          nWrong={nWrong}
+          rapper={rapper}
+          winChecker={winChecker}
+          handleGuess={handleGuess}
+        />
 
+      </div>
+
+      <div id="game">
+        <footer>
+          <a href="https://github.com/luco17/hangman2" target="_blank">
+            <img src={githubLogo} alt="sharing logo" />
+          </a>
+        </footer>
+      </div>
     </div>
   )
 }
